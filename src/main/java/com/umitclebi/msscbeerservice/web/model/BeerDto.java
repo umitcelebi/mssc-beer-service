6a0581681c90ada2b619901f003aa35fddcdd3c6
umitclebi <ucelebi@avansas.com>
2022-03-29 10:46:57 +0300

    adding beer controller. closes #3

--- src/main/java/com/umitclebi/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/umitclebi/msscbeerservice/web/model/BeerDto.java
@@ -1,9 +1,18 @@
 package com.umitclebi.msscbeerservice.web.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BeerDto {
     private UUID id;
     private Integer version;
